a257402081eebb3ac269ced438bda37eef6c1dc6
Polo <moogiipolo@gmail.com>
2021-01-29 06:35:46 +0800

    hghg

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Сайн байна уу <a href="https://nextjs.org">Next.js!</a>
+          Сайн байна уу <a href="https://nextjs.org">uzlee haha</a>
         </h1>
 
         <p className={styles.description}>
